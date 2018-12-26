8cc4798ad7268e47f53ad58d2fe01c2105a9c34e
Hugo <courtois.hugo@gmail.com>
2018-12-26 16:49:57 +0100

    Added shortcuts

--- install_work_config.bash
+++ install_work_config.bash
@@ -1,24 +1,75 @@
 #!/bin/bash
 
-git clone https://github.com/HuCourt/work_install.git
-cd work_install
-ln -s $(pwd)/.vimrc ~/.vimrc 
-ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+install_package() {
+PKG_INSTALLED=$(dpkg-query -W --showformat='${Status}\n' "$1" | grep "install ok installed")
+if [ "$PKG_INSTALLED" == "" ];
+then
+    echo "$1 not found. Installing ..."
+    sudo apt-get --yes install "$1";
+fi
+}
 
-mkdir ~/.vim
-mkdir ~/.vim/autoload
-mkdir ~/.vim/bundle
+add_kb() {
+#Add keybinding to ubuntu
+KB_NAME=$1
+KB_COMMAND=$2
+KB_BIND=$3
+
+LIST_KB=$(gsettings get org.gnome.settings-daemon.plugins.media-keys custom-keybindings)
+LIST_KB=${LIST_KB#"@as ["}
+LIST_KB=${LIST_KB#"["}
+LIST_KB=${LIST_KB%"]"}
+LIST_KB=$(echo $LIST_KB | tr -d '[:space:]')
+
+contains () {
+    [[ $1 =~ (^|',')$2($|',') ]] && return 0 || return 1
+}
+
+KEY="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom"
+IDX=0
+new=""
+while true; do
+    new="'"${KEY}${IDX}/"'"
+    
+    if contains "${LIST_KB}" "${new}" ;
+    then
+        IDX=$((IDX+1))
+    else
+        break
+    fi
+done
+if [ ${IDX} -eq 0 ] ;
+then
+    separator=""
+else
+    separator=","
+fi
+    
+LIST_KB="@as "["$LIST_KB""$separator""$new"]
+
+kb_init_command="gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom${IDX}/"
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "${LIST_KB}"
+${kb_init_command} name "${KB_NAME}"
+${kb_init_command} command "${KB_COMMAND}"
+${kb_init_command} binding "${KB_BIND}"
+
+}
+ln -sf $(pwd)/.vimrc ~/.vimrc 
+ln -sf $(pwd)/.tmux.conf ~/.tmux.conf
+
+mkdir -p ~/.vim/autoload
+mkdir -p ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 
-if [ $(dpkg-query -W --showformat='${Status}\n' vim |grep "install ok installed") -eq 0 ];
-then
-    echo "Vim not found. Installing ..."
-    sudo apt-get --yes install vim;
-fi
 
-if [ $(dpkg-query -W --showformat='${Status}\n' tmux |grep "install ok installed") -eq 0 ];
-then
-    echo "Tmux not found. Installing ..."
-    sudo apt-get --yes install tmux;
-fi
+install_package vim
+#Terminal multiplexer
+install_package tmux
+#For bringing to forefront
+install_package wmctrl
+
+
+add_kb "Bring_browser_ff" "$(pwd)/bring_forefront.sh firefox" "<Control>b"
+add_kb "Bring_terminal_ff" "$(pwd)/bring_forefront.sh gnome-terminal-server" "<Control>t"
